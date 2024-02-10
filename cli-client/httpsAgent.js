c383cacda6b4228bc31e0e0b97b3edd9bac96075
ntua-el20063 <el20063@mail.ntua.gr>
2024-02-11 00:19:55 +0200

    a small change in certificate paths

--- cli-client/httpsAgent.js
+++ cli-client/httpsAgent.js
@@ -2,12 +2,17 @@ import https from 'https';
 import fs from 'fs';
 import path from 'path';
 
+/*
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
 const certPath = path.join(__dirname, '../../softeng23-07/back-end/sslcert/server.crt');
 const keyPath = path.join(__dirname, '../../softeng23-07/back-end/sslcert/server.key');
 const caPath = path.join(__dirname, '../../softeng23-07/back-end/sslcert/rootCA.pem');
+*/
 
+const certPath = '../back-end/sslcert/server.crt';
+const keyPath = '../back-end/sslcert/server.key';
+const caPath = '../back-end/sslcert/rootCA.pem';
 
 const httpsAgent = new https.Agent({
     rejectUnauthorized: true, 
